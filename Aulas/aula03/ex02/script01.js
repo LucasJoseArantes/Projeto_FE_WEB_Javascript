3500c4b201bd388a46178a9501dec3a531485052
Rafael Prado <lucas.js@estudante.iftm.edu.br>
2024-03-13 22:57:01 -0300

    Update 2

--- Aulas/aula03/ex02/script01.js
+++ Aulas/aula03/ex02/script01.js
@@ -2,15 +2,29 @@ window.addEventListener('load', () => {
 
     document.getElementById("cep").addEventListener("blur", () => { // blur é o evento que ocorre quando o campo perde o foco;
 
+    var cepMsg = document.getElementById("cepMsg");
+
         var cep = document.getElementById("cep").value.replace(/\D/g, ''); // Expressão regular para encontrar não dígitos;
+
 
         if (cep.length == 8) {
             fetch(`https://viacep.com.br/ws/${cep}/json/`)
                 .then(response => response.json())
                 .then(data => {
-                    document.getElementById("rua").value = data.logradouro;
-                    document.getElementById("cidade").value = data.localidade;
-                    document.getElementById("bairro").value = data.bairro;
+
+                    if(  data.logradouro == undefined || data.localidade == undefined || data.bairro == undefined){
+                        cepMsg.innerHTML = "CEP não encontrado" ;
+                        document.getElementById("rua").value = ""
+                        document.getElementById("cidade").value = ""
+                        document.getElementById("bairro").value=  ""
+                    }
+                    else{
+                        document.getElementById("rua").value = data.logradouro;
+                        document.getElementById("cidade").value = data.localidade;
+                        document.getElementById("bairro").value = data.bairro;
+                        cepMsg.innerHTML = "" ;
+                    }
+                    
                 });
         }
 

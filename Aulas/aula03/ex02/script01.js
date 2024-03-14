e24731650b00da42735ec7f79a375442206f6e11
Rafael Prado <lucas.js@estudante.iftm.edu.br>
2024-03-13 22:42:07 -0300

    Update Aula02

--- Aulas/aula03/ex02/script01.js
+++ Aulas/aula03/ex02/script01.js
@@ -1,20 +1,16 @@
 window.addEventListener('load', () => {
 
-    document.getElementById("cep").addEventListener("blur", () => {
+    document.getElementById("cep").addEventListener("blur", () => { // blur é o evento que ocorre quando o campo perde o foco;
 
-        var cep = document.getElementById("cep").value;
-        cep = cep.replace(/\D/g, ''); // Expressão regular para encontrar não dígitos
-
-        
-        var rua = document.getElementById("rua");
-        var cidade = document.getElementById("cidade");
+        var cep = document.getElementById("cep").value.replace(/\D/g, ''); // Expressão regular para encontrar não dígitos;
 
         if (cep.length == 8) {
             fetch(`https://viacep.com.br/ws/${cep}/json/`)
                 .then(response => response.json())
                 .then(data => {
-                    rua.value = data.logradouro;
-                    cidade.value = data.localidade;
+                    document.getElementById("rua").value = data.logradouro;
+                    document.getElementById("cidade").value = data.localidade;
+                    document.getElementById("bairro").value = data.bairro;
                 });
         }
 

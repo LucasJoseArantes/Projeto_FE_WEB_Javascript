34dabcfb1bd5fc1fd936913ec1d74d5125dba181
Lucas <lucas.js@estudante.iftm.edu.br>
2024-07-10 18:22:07 -0300

    UPDATE: Gh pages

--- Listas/lista05/Exemple01/vite.config.js
+++ Listas/lista05/Exemple01/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/Projeto_FE_WEB_Javascript"
+  base: "/Projeto_FE_WEB_Javascript/Listas/lista05/Exemple01/"
 })
